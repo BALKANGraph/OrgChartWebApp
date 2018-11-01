32acddc4615e1b87b998df792479f66ccbe746bf
BALKANGraph <plamen.peshev@live.com>
2018-11-02 00:08:00 +0200

    update

--- OrgChartWebApp/App_Start/RouteConfig.cs
+++ OrgChartWebApp/App_Start/RouteConfig.cs
@@ -13,7 +13,7 @@ namespace OrgChartWebApp
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            routes.MapRoute( 
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional }
