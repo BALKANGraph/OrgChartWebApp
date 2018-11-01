c12070dfbaf86450aa2fde767c5bb2d09aa1d79e
BALKANGraph <plamen.peshev@live.com>
2018-11-02 00:18:11 +0200

    update

--- OrgChartWebApp/App_Start/RouteConfig.cs
+++ OrgChartWebApp/App_Start/RouteConfig.cs
@@ -10,7 +10,7 @@ namespace OrgChartWebApp
     public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
-        {
+        { 
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute( 
