123c0a3a3bdff9363de5b93410a476771c714ac9
balkangraph <zornitsa.pesheva@outlook.com>
2023-04-23 17:34:39 +0300

    updated to work

--- OrgChartWebApp/DAL/OrgChartModel.Designer.cs
+++ OrgChartWebApp/DAL/OrgChartModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Temp\chart\OrgChartWebApp\OrgChartWebApp\DAL\OrgChartModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Working\.net\OrgChartWebApp\OrgChartWebApp\DAL\OrgChartModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
